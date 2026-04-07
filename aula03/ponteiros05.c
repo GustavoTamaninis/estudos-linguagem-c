3964306003fd098732c428584802fb2870c2a753
GustavoTamaninis <194152443+GustavoTamaninis@users.noreply.github.com>
2026-04-07 17:45:40 -0300

    feat: passagem de ponteiros como parâmetros em funções

---
+++ aula03/ponteiros05.c
@@ -0,0 +1,21 @@
+//Passagem de ponteiros como parâmetros em funções:
+#include <stdio.h>
+void adiciona1PorValor(int valor) {
+    valor++;
+    printf("Argumento valor = %d\n", valor);
+}
+
+void adiciona1PorReferencia(int *valor) {
+    (*valor)++;
+    printf("Argumento valor = %d\n", *valor);
+}
+
+int main() {
+    int valor_original = 100;
+    adiciona1PorValor(valor_original);
+    printf("Valor_original = %d\n\n", valor_original);
+
+    adiciona1PorReferencia(&valor_original);
+    printf("Valor_original = %d\n", valor_original);
+    return 0;
+}
