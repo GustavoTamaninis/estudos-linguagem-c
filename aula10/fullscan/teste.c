a0076eff1a35e2758140fcb615f57ad4e3235e2b
GustavoTamaninis <194152443+GustavoTamaninis@users.noreply.github.com>
2026-04-24 16:38:30 -0300

    feat: aula10 fullscan múltiplo ordenado

--- aula10/fullscan/teste.c
+++ aula10/fullscan/teste.c
@@ -27,6 +27,18 @@ void fullscan_multiplo(int * v, int * tamanho, int dado_proc, int * indices){
     *tamanho = encontrados;
 }
 
+void fullscan_multiplo_ordenado(int * v, int * tamanho, int dado_proc, int * indices){
+    int encontrados = 0;
+    for(int j = 0; j < *tamanho; j++){
+        if(v[j] > dado_proc) break;
+        if(v[j] == dado_proc){
+            indices[encontrados] = j;
+            encontrados++;
+        }
+    }
+    *tamanho = encontrados;
+}
+
 int main(){
     int vetor[6] = {4, 5, 2, -4, 9, -4};
     for(int i = 0; i < 6; i++){
@@ -39,11 +51,27 @@ int main(){
     printf("\nAchado o valor %d no índice %d...", dado_proc, fullscan(vetor, tamanho, dado_proc));
 
     printf("\nAchado o valor %d nos índices: ", dado_proc);
-
     int indices[tamanho];
     fullscan_multiplo(vetor, &tamanho, dado_proc, indices);
     for(int i = 0; i < tamanho; i++){
         printf("%d | ", indices[i]);
+    }
+
+    vetor[0] = 5;
+    vetor[1] = 8;
+    vetor[2] = 10;
+    vetor[3] = 12;
+    vetor[4] = 12;
+    vetor[5] = 13;
+
+    dado_proc = 12;
+    tamanho = 6;
+    int indices_2[tamanho];
+    printf("\nAchado o valor %d nos índices: ", dado_proc);
+    fullscan_multiplo_ordenado(vetor, &tamanho, dado_proc, indices_2);
+    printf("\nTamanho %d\n", tamanho);
+    for(int i = 0; i < tamanho; i++){
+        printf("%d | ", indices_2[i]);
     }
 }
 
